98d8a1e280a65b6c86e25d58d73008cd45655212
Raj Sharma <sharmaraj.sm@gmail.com>
2025-10-28 19:35:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import os
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+database_path = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+if database_path.startswith('sqlite'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+
 db = SQLAlchemy(app)
 
 from flask_cors import CORS
